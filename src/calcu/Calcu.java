808aa9d492bd67fa9dbc1477da9c0b7fc7d26043
USUARIO <USUARIO@USUARIO-PC>
2015-01-29 23:50:27 -0500

    1# inicio del proyecto

--- src/calcu/Calcu.java
+++ src/calcu/Calcu.java
@@ -17,7 +17,7 @@ public class Calcu {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo");
+        System.out.println("hola mundooooo");
     }
     
 }
